3121141c104f27f6053281e3dbaf5d267758ac49
Yevhen Strohanov <yevhen.strohanov@fiserv.com>
2019-08-29 16:41:40 -0500

    using find_element for android platform textfield search

--- lib/input_valet.rb
+++ lib/input_valet.rb
@@ -3,9 +3,16 @@ require_relative 'ui_valet'
 module AppiumValet
   class InputValet < UIValet
     def into(textfield:)
-      input_field = driver.textfield(textfield)
+      input_field = textfield_element(textfield)
       input_field.clear
       input_field.send_keys(@selector_text)
+    end
+
+    private
+
+    def textfield_element(search_value)
+      return driver.textfield(search_value) if driver.device_is_ios?
+      return driver.find_element(id: search_value) if driver.device_is_android?
     end
   end
 end
